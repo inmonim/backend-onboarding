925ca21058558e5b5810e4e2ccb58cb8de6c419d
inmonim <cat1083014@gmail.com>
2025-01-07 13:09:02 +0900

    feat : base viewsets 구성

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+from django.contrib.auth.hashers import make_password
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    def create(self, request, *args, **kwargs):
+        hashed_password = make_password(request.data['password'])
+        request.data['password'] = hashed_password
+        return super().create(request, *args, **kwargs)
